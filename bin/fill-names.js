9ae7de86ad886e51a3d14ca52a9ade47eef9688e
Olmo Maldonado <olmo.maldonado@gmail.com>
2016-03-22 15:05:30 -0500

    channels can be updated.

--- bin/fill-names.js
+++ bin/fill-names.js
@@ -6,6 +6,8 @@ if (false && /rhc/.test(os.hostname())){
 
 var config = require('../lib/config.js');
 
+var BOT_USERS = config('IGNORE_BOT_USERS');
+
 var Botkit = require('botkit');
 var firebase = require('../storage/firebase.js');
 
@@ -18,30 +20,52 @@ var controller = Botkit.slackbot({
 var bot = controller.spawn({ token: config('TOKEN') });
 var storage = controller.storage;
 
-var MAP = {
-	C: 'channel',
-	D: 'user',
-	G: 'group'
-}
-
 storage.channels.all(function(err, channels){
 	
 	channels.forEach(function(channel){
-		if (channel.name) return;
+		if (channel.id.charAt(0) != 'D' && channel.name) return;
 
-		var type = MAP[channel.id.charAt(0)];
-		if (!type) return console.error('No type found for: ' + channel.id);
+		switch (channel.id.charAt(0)){
+			case 'C':
+				bot.api.channels.info({channel: channel.id}, function(err, res){
+					if (err) return;
+					channel.name = res.channel.name;
+					storage.channels.save(channel);
+				});
+				break;
+			case 'D':
+				bot.api.im.history({channel: channel.id}, function(err, res){
+					if (err) return;
 
-		var options = {};
-		options[type] = channel.id;
-		bot.api[type + 's'].info(options, function(err, res){
-			if (err) return console.error(res);
-			if (!res[type].name) return console.error('no name: ' + res[type].name + ' channel: ' + channel.id);
+					// don't include taskbot(s)
+					var message = res.messages.filter(function(message){
+						return BOT_USERS.indexOf(message.user) < 0;
+					})[0];
 
-			channel.name = res[type].name;
-			storage.channels.save(channel);
-		});
-
+					storage.users.get(message.user, function(err, user){
+						if (!user){
+							bot.api.users.info({user: message.user}, function(err, res){
+								if (err) return;
+								storage.users.save(res.user);
+								channel.name = res.user.name;
+								storage.channels.save(channel);
+							});
+						} else {
+							channel.name = user.name;
+							storage.channels.save(channel);
+						}
+					});
+				});
+			case 'G':
+				bot.api.groups.info({channel: channel.id}, function(err, res){
+					if (err) return;
+					channel.name = res.group.name;
+					storage.channels.save(channel);
+				});
+				break;
+			default:
+				console.error('unknown type for: ' + channel.id)
+		}
 	});
 
 });
